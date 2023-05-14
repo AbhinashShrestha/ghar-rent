25563df71ba273b95a9db0910387697d989967e7
owner <oredev@proton.me>
2023-05-14 16:59:19 +0545

    fixed production error

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,4 +1,50 @@
-'use client';
+// 'use client';
+// import Container from "../Container";
+// import Categories from "./Categories";
+// import Logo from "./Logo";
+// import Search from "./Search";
+// import UserMenu from "./UserMenu";
+// import { SafeUser } from "@/app/types";
+
+
+// interface NavbarProps {
+//     currentUser?: SafeUser | null;
+// }
+
+// const Navbar: React.FC<NavbarProps>=({
+//     currentUser
+// })=>{ 
+//     return (
+//         <div className="fixed w-full bg--white z-10 shadow-sm">
+//             <div className="
+//                 py-4
+//                 border-b-[1px]
+//             "
+//             >
+//             <Container>
+//                 <div
+//                 className="
+//                 flex
+//                 flex-row
+//                 items-center
+//                 justify-between
+//                 gap-3
+//                 md:gap-0
+                
+//                 "
+//                 >
+//                     <Logo/>
+//                     <Search/>
+//                     <UserMenu currentUser={currentUser} />
+//                 </div>
+//             </Container>
+//             </div>
+//             <Categories />
+//         </div>
+//     )
+// }
+
+// export default Navbar;
 import Container from "../Container";
 import Categories from "./Categories";
 import Logo from "./Logo";
@@ -6,42 +52,38 @@ import Search from "./Search";
 import UserMenu from "./UserMenu";
 import { SafeUser } from "@/app/types";
 
-
 interface NavbarProps {
-    currentUser?: SafeUser | null;
+ currentUser?: SafeUser | null;
 }
 
-const Navbar: React.FC<NavbarProps>=({
-    currentUser
-})=>{ 
-    return (
-        <div className="fixed w-full bg--white z-10 shadow-sm">
-            <div className="
-                py-4
-                border-b-[1px]
-            "
-            >
-            <Container>
-                <div
-                className="
-                flex
-                flex-row
-                items-center
-                justify-between
-                gap-3
-                md:gap-0
-                
-                "
-                >
-                    <Logo/>
-                    <Search/>
-                    <UserMenu currentUser={currentUser} />
-                </div>
-            </Container>
-            </div>
-            <Categories />
-        </div>
-    )
-}
+const Navbar: React.FC<NavbarProps> = ({
+ currentUser
+}) => {
+ return (
+ <div className="fixed w-full bg--white z-10 shadow-sm">
+ <div className="
+ py-4
+ border-b-[1px]
+ ">
+ <Container>
+ <div
+ className="
+ flex
+ flex-row
+ items-center
+ justify-between
+ gap-3
+ md:gap-0
+ ">
+ <Logo />
+ <Search />
+ {currentUser !== undefined && <UserMenu currentUser={currentUser} />}
+ </div>
+ </Container>
+ </div>
+ <Categories />
+ </div>
+ );
+};
 
 export default Navbar;
